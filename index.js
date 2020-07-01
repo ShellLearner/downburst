fcacec7780ef516cbb713ab69b9113e479dd4464
ShellLearner <59457583+ShellLearner@users.noreply.github.com>
2020-07-01 11:36:04 -0400

    added something

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("../config.json");
-
+const config = require("./config.json");
+const prefix = config.prefix
 
 client.on("ready", () => {
 	console.log("Bot is online!");
+});
+
+client.on("message", message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) {return};
+	//prevents bots calling each other + messages not containing the prefix ignored
+	
+ 	if 
 });
 
 client.login(config.token);
