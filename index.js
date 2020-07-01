dcb9778934d50692a47d56ed69f0eaeeac7d48c2
Logan Do <logando@ado.local>
2020-07-01 12:35:58 -0400

    Streamlined code, added foundation for commands

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ client.on("ready", () => {
 });
 
 client.on("message", message => {
-	
+
 	//prevents bots calling each other and
 	//messages not containing the prefix ignored
-	if (!message.content.startsWith(prefix) || message.author.bot) {
-		console.log ("ignored message");
-		return
-	};
-	
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/\ +/g);
+	const command = args.shift().toLowerCase();
+
+	if (command === ping) {
+		console.log("Pong! I'm alive, don't worry. Yet.")
+	}
 });
 
 client.login(config.token);
