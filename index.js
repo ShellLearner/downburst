8cbc1640fee05689f9372603254fc4098af53097
Logan Do <logando@ado.local>
2020-07-09 13:21:06 -0400

    added crazy stuff: new kind of command handler

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-const prefix = config.prefix
-const sleep = require("./sleep.js")
+const prefix = config.prefix;
+const fs = require("fs")
 
 
 client.on("ready", () => {
@@ -19,18 +19,37 @@ client.on("message", async (message) => {
 	const args = message.content.slice(prefix.length).trim().split(/\ +/g);
 	const command = args.shift().toLowerCase();
 
+	/* NOTE: untested
+	if (fs.existsSync(`commands/${command}.js`)) {
+		const cmd = require(`commands/${command}.js`);
+		cmd(args);
+	}	else {
+	message.channel.send("Sorry, this command does not exist.")
+	return;
+	};
+
+	*/
+
 	if (command === "ping") {
 		message.channel.send("Pong! I'm alive, don't worry. Yet.")
-	}
+	};
 
 	if (command === "whoami"){
+		//it's just easier to load the sleep function here
+		const sleep = require("./sleep.js");
+
 		message.channel.send(`Hmm. Let me check.`);
 		message.channel.startTyping(2);
 		await sleep(5000)
 		message.channel.send(`It appears you're ${message.author.username}.`);
 		message.channel.stopTyping(true);
+	};
 
-	}
+	if (command === "help"){
+		console.log("help page called, hurry up and make one")
+		message.channel.send("Hi! We do not currently have a help page configured. ~~Also there are only two commands, ping and whoami, so chill.~~");
+	};
+
 });
 
 client.login(config.token);
--- sleep.js
+++ sleep.js
@@ -1,6 +1,7 @@
 //A nice sleep function! Taken from user
 //https://stackoverflow.com/users/1269037/dan-dascalescu
 //in stackoverflow.
+// WARNING: only works in an async function!
 
 module.exports = ms => {
   return new Promise(resolve => setTimeout(resolve, ms));
